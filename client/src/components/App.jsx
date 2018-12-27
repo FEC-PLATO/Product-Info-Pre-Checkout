eb0dce970cb6cbe3add4eb4d98fd9baff6279a1f
ryanwlaw <ryanwlaw30@gmail.com>
2018-12-27 12:44:34 -0800

    star rating is dynamically rendering based on averageRating from DB

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StarRatings from 'react-star-ratings';
 
 class App extends React.Component {
 
@@ -11,14 +12,33 @@ class App extends React.Component {
   }
 
   componentDidMount () {
+    console.log('LOCATION', window.location);
+    // var currentLocation = window.location.assign('http://localhost:3000/api/item/1');
+    // window.location.href = 'http://localhost:3000/1';
+    fetch('/api/item/1')
+      .then(res => res.json())
+      .then(data => this.setState({items: data}))
   }
 
   render() {
 
+    var priceStyle = {
+      fontSize: "50px",
+      padding: 20,
+      margin: 10
+    }
+
     return (
       <div>
-        <div>{this.state.items.price}</div>
+        <div style={priceStyle}>{this.state.items.price}</div>
         <div>
+          <StarRatings
+            rating={this.state.items.averageRating}
+            starDimension="20px"
+            starSpacing="2px"
+            starRatedColor="yellow"
+            numberOfStars={5}
+          />
           <span>{this.state.items.averageRating} </span>
           <span>{this.state.items.totalRatings} </span>
           <span>{this.state.items.totalQuestions} Questions</span>
