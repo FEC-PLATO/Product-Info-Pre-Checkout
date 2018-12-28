0b9e7b262a9ebb835ef024b1a8e2113f95eff08c
ryanwlaw <ryanwlaw30@gmail.com>
2018-12-27 17:34:08 -0800

    working color and size components. will need to refactor mapping each record from db

---
+++ client/src/components/Color.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+class Color extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayColor: '',
+      borderColor: ''
+    }
+  }
+
+  onClickChange (event) {
+    this.setState({displayColor: event});
+  }
+
+  onMouseEnterChange(event) {
+    this.setState({displayColor: event});
+    console.log(event);
+  }
+
+  onMouseLeaveChange(event) {
+    this.setState({displayColor: ''});
+    console.log(event)
+  }
+
+  render() {
+
+    var colorBlack = {
+      backgroundColor: "black"
+    }
+    var colorSilver = {
+      backgroundColor: "silver"
+    }
+    var colorGold = {
+      backgroundColor: "gold"
+    }
+    var colorPink = {
+      backgroundColor: "pink"
+    }
+
+    var colorBox = {
+      width: "40px",
+      height: "40px",
+      border: "",
+      borderRadius:"5px",
+      display: "inline-block",
+      margin: 5
+    }
+
+    return (
+      <div>
+        <div>Color: {this.state.displayColor}</div>
+        <div
+          style={Object.assign(colorBlack, colorBox)}
+          onClick= {() => this.onClickChange('Black')}
+          onMouseEnter={() => this.onMouseEnterChange('enter')}
+          onMouseLeave={() => this.onMouseLeaveChange('leave')}></div>
+        <div
+          style={Object.assign(colorSilver, colorBox)}
+          onClick= {() => this.onClickChange('Silver')}></div>
+        <div
+          style={Object.assign(colorGold, colorBox)}
+          onClick= {() => this.onClickChange('Gold')}></div>
+        <div
+          style={Object.assign(colorPink, colorBox)}
+          onClick= {() => this.onClickChange('Pink')}></div>
+      </div>
+    )
+  }
+}
+
+export default Color;
---
+++ client/src/components/Size.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class Size extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displaySize: ''
+    }
+  }
+
+  onClickSize (event) {
+    this.setState({displaySize: event})
+  }
+
+  render() {
+
+    var sizeStyle = {
+      display: "inline-block",
+      fontSize: "15px",
+      borderStyle: "solid",
+      borderRadius: "5px",
+      borderWidth: "1px",
+      margin: 5,
+      padding: 5
+    }
+
+    return (
+      <div>
+        <div>Size: {this.state.displaySize}</div>
+        <div style={sizeStyle} onClick={() => this.onClickSize('64GB')}>64GB</div>
+        <div style={sizeStyle} onClick={() => this.onClickSize('256GB')}>256GB</div>
+        <div style={sizeStyle} onClick={() => this.onClickSize('512GB')}>512GB</div>
+      </div>
+    )
+  }
+}
+
+
+export default Size;
