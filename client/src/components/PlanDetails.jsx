71208c5a975a6793a486567590a14c737106f9e8
ryanwlaw <ryanwlaw30@gmail.com>
2019-01-01 17:31:07 -0800

    created a close button for modal

--- client/src/components/PlanDetails.jsx
+++ client/src/components/PlanDetails.jsx
@@ -23,17 +23,26 @@ class PlanDetails extends React.Component {
 
     var checkBoxStyle = {
       height: "15px",
-      width: "15px"
+      width: "15px",
+      cursor: "pointer"
+    }
+
+    var closeStyle = {
+      float: "right",
+      padding: 10,
+      fontSize: "25px",
+      cursor: "pointer"
     }
 
     return(
       <div style={{fontFamily: "Verdana", fontSize:"12px"}}>
         <input type="checkbox" style={checkBoxStyle}/>
         <span>2 year Protection Plan with Accidental Damage Coverage </span>
-        <span onClick={this.openModal.bind(this)} style={{fontSize:"10px", textDecoration: "underline"}}>See plan details</span>
+        <span onClick={this.openModal.bind(this)} style={{fontSize:"10px", textDecoration: "underline", cursor: "pointer"}}>See plan details</span>
         <Modal visible={this.state.show} width="500" height="400" effect="fadeInUp" onClickAway={this.closeModal.bind(this)}>
+          <div onClick={this.closeModal.bind(this)} style={closeStyle}>&#10006;</div>
           <div>
-            <h1>See plan details</h1>
+            <h3>See plan details</h3>
             <p>Contract Delivery: Email</p>
             <p>Benefits: Free 2-way shipping, 24/7 support, no additional cost, no deductible</p>
             <p>Support: Phone, online</p>
@@ -42,7 +51,6 @@ class PlanDetails extends React.Component {
             <p>Maximum Qualifying Item Price: 449.99</p>
             <p>Minimum Qualifying Item Price: 400</p>
             <p>Number of Years Protected: 2 years</p>
-            <div onClick={this.closeModal.bind(this)}>Close</div>
           </div>
         </Modal>
       </div>
